6d37eb18da527a50e1ca3de164e7fc9e57d2ab85
ipetrash <ilya.petrash@inbox.ru>
2020-07-30 00:55:47 +0500

    Upgrading parser quotes Refactoring bot answers

--- main.py
+++ main.py
@@ -7,16 +7,14 @@ __author__ = 'ipetrash'
 import html
 import logging
 import sys
-from urllib.request import urlopen, Request
-from urllib.parse import urljoin
-
-import config
-
-from bs4 import BeautifulSoup
+from typing import List
 
 # pip install python-telegram-bot
 from telegram.ext import Updater, MessageHandler, CommandHandler, Filters, CallbackContext
 from telegram import ReplyKeyboardMarkup, Update
+
+import config
+from bash_im import Quote, get_random_quotes_list
 
 
 def get_logger():
@@ -45,66 +43,38 @@ log = get_logger()
 
 # Хранилище цитат башорга, из которого будут браться цитаты, и посылаться в телеграм.
 # Когда этот список будет пустым, оно будет заполнено с сайта.
-QUOTES_LIST = list()
+QUOTES_LIST: List[Quote] = []
 
 REPLY_KEYBOARD_MARKUP = ReplyKeyboardMarkup(
     [['Хочу цитату!']], resize_keyboard=True
 )
 
 
-# SOURCE: https://github.com/gil9red/SimplePyScripts/blob/511a9fb408d8e8e470ce7f3ea4bcbf8d632e10a6/random_quote_bashim.py#L16
-def get_random_quotes_list():
-    log.debug('get_random_quotes_list')
-
-    quotes = []
-
-    try:
-        with urlopen(Request(config.URL, headers={'User-Agent': config.USER_AGENT})) as f:
-            root = BeautifulSoup(f.read(), 'html.parser')
-
-            # Remove comics
-            for x in root.select('.quote__strips'):
-                x.decompose()
-
-            for quote_el in root.select('.quote'):
-                try:
-                    href = quote_el.select_one('.quote__header_permalink')['href']
-                    url = urljoin(config.URL, href)
-                    quote_text = quote_el.select_one('.quote__body').get_text(separator='\n', strip=True)
-                    quotes.append((quote_text, url))
-                except IndexError:
-                    pass
-
-    except Exception as e:
-        log.exception(f'{e} + \n\nQuote:\n{quote_el}')
-
-    return quotes
-
-
-def get_random_quote():
+def get_random_quote() -> Quote:
     log.debug('get_random_quote (QUOTES_LIST: %s)', len(QUOTES_LIST))
 
-    quote_text, url = None, None
+    quote = None
 
     try:
         # Если пустой, запрос и заполняем список новыми цитатами
         if not QUOTES_LIST:
             log.debug('QUOTES_LIST is empty, do new request.')
-            QUOTES_LIST.extend(get_random_quotes_list())
+            QUOTES_LIST.extend(get_random_quotes_list(log))
 
             log.debug('New quotes: %s.', len(QUOTES_LIST))
 
-        quote_text, url = QUOTES_LIST.pop()
+        quote = QUOTES_LIST.pop()
 
     except Exception as e:
         log.exception(e)
 
-    return quote_text, url
+    return quote
 
 
-def get_html_message(text: str, url: str) -> str:
-    link = f"""<a href="{url}">#{url.split('/')[-1]}</a>"""
-    return html.escape(text) + '\n\n' + link
+def get_html_message(quote: Quote) -> str:
+    link = f"""<a href="{quote.url}">#{quote.id}</a>"""
+    text = html.escape(quote.text)
+    return f'{text}\n\n{quote.date_str} | {link}'
 
 
 def error_callback(update: Update, context: CallbackContext):
@@ -122,20 +92,20 @@ def work(update: Update, context: CallbackContext):
     log.debug('work[chat_id=%s]', chat_id)
 
     try:
-        text, url = get_random_quote()
+        quote = get_random_quote()
         if config.LOG_QUOTE_TEXT:
-            log.debug('Quote text (%s):\n%s', url, text)
+            log.debug('Quote text (%s):\n%s', quote.url, quote.text)
         else:
-            log.debug('Quote text (%s)', url)
+            log.debug('Quote text (%s)', quote.url)
 
-        if not text:
+        if not quote:
             log.warning("Don't receive quote...")
             update.message.reply_text(config.ERROR_TEXT)
             return
 
         # Отправка цитаты и отключение link preview -- чтобы по ссылке не генерировалась превью
         update.message.reply_html(
-            get_html_message(text, url),
+            get_html_message(quote),
             disable_web_page_preview=True,
             reply_markup=REPLY_KEYBOARD_MARKUP
         )
