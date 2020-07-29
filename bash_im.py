faae54dd340ba8a04b9f47ce05bd527445bd8292
ipetrash <ilya.petrash@inbox.ru>
2020-07-30 03:37:29 +0500

    Upgrading parser quotes

--- bash_im.py
+++ bash_im.py
@@ -4,7 +4,7 @@
 __author__ = 'ipetrash'
 
 
-# SOURCE: https://github.com/gil9red/SimplePyScripts/blob/c2a786b3cce3278d0fe2b255e4a07164c267962e/html_parsing/random_quote_bash_im/bash_im.py
+# SOURCE: https://github.com/gil9red/SimplePyScripts/blob/3e645cee104703779d2a2aac8dd7ae5ba2821af2/html_parsing/random_quote_bash_im/bash_im.py
 
 
 import datetime as DT
@@ -13,10 +13,11 @@ from urllib.request import urlopen, Request
 from urllib.parse import urljoin
 from typing import List, Union
 import traceback
+from pathlib import Path
 import re
 
-
 from bs4 import BeautifulSoup, Tag
+import requests
 
 
 # SOURCE: https://github.com/gil9red/SimplePyScripts/blob/9c9a5555e87d7a7475c2dc8feffb97f2906789ce/shorten.py#L7
@@ -50,14 +51,58 @@ class Quote:
         self._id = int(self.url.rstrip().split('/')[-1])
         self._date_str = self.date.strftime('%d.%m.%Y')
 
+    def download_comics(self, dir_name: Union[str, Path] = None, logger=None) -> List[str]:
+        if isinstance(dir_name, str):
+            dir_name = Path(dir_name)
+
+        if dir_name is None:
+            dir_name = Path(f'comics/quote_{self.id}')
+
+        files = []
+
+        for url in self.comics_url:
+            try:
+                dir_name.mkdir(parents=True, exist_ok=True)
+
+                comics_id = url.rstrip('/').split('/')[-1]
+                file_name = dir_name / f'{comics_id}.png'
+
+                # Если нет файла, скачиваем
+                if not file_name.exists():
+                    session = requests.session()
+                    session.headers['User-Agent'] = USER_AGENT
+
+                    # Страница комикса
+                    rs = session.get(url)
+                    root = BeautifulSoup(rs.content, 'html.parser')
+
+                    img_el = root.select_one('.quote__img')
+                    url_src = img_el.get('src') or img_el.get('data-src')
+                    url_img = urljoin(URL_BASE, url_src)
+
+                    # Картинка комикса
+                    rs = session.get(url_img)
+                    file_name.write_bytes(rs.content)
+
+                files.append(str(file_name.resolve()))
+
+            except Exception:
+                msg = f'Error by parsing comics {url}:\n\n'
+                if logger:
+                    logger.exception(msg)
+                else:
+                    print(f'{msg}{traceback.format_exc()}')
+
+        return files
+
     @staticmethod
     def parse_from(url_or_el: Union[str, Tag]) -> 'Quote':
         if isinstance(url_or_el, str):
             url = url_or_el
 
-            with urlopen(Request(url, headers={'User-Agent': USER_AGENT})) as f:
-                root = BeautifulSoup(f.read(), 'html.parser')
-                quote_el = root.select_one('article.quote')
+            rs = requests.get(url, headers={'User-Agent': USER_AGENT})
+            root = BeautifulSoup(rs.content, 'html.parser')
+            quote_el = root.select_one('article.quote')
         else:
             quote_el = url_or_el
 
@@ -168,3 +213,11 @@ if __name__ == '__main__':
     #  23. Quote(id=217468, url=https://bash.im/quote/217468, text(687)='*****:\nНастроил в квартире сет...', date=13.05.2007, rating=5847, comics_url=[])
     #  24. Quote(id=404924, url=https://bash.im/quote/404924, text(279)='Воланд: Дорогая Лиза, я понима...', date=30.10.2009, rating=8788, comics_url=[])
     #  25. Quote(id=417637, url=https://bash.im/quote/417637, text(166)='xxx: Блин. Нормальные люди, ко...', date=26.06.2012, rating=7810, comics_url=[])
+
+    print()
+
+    quote = Quote.parse_from('https://bash.im/quote/414617')
+    files = quote.download_comics()
+    print(f'Files ({len(files)}):')
+    for i, file_name in enumerate(files, 1):
+        print(f'  {i:2}. {file_name}')
