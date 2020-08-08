7108e8a329c4fb47ed677299c93b30a628866f1f
ipetrash <ilya.petrash@inbox.ru>
2020-08-08 06:31:57 +0500

    Refactoring

--- config.py
+++ config.py
@@ -5,9 +5,13 @@ __author__ = 'ipetrash'
 
 
 import os
+from pathlib import Path
 
 
-TOKEN = os.environ.get('TOKEN') or open('TOKEN.txt', encoding='utf-8').read().strip()
+DIR = Path(__file__).resolve().parent
+TOKEN_FILE_NAME = DIR / 'TOKEN.txt'
+
+TOKEN = os.environ.get('TOKEN') or TOKEN_FILE_NAME.read_text('utf-8').strip()
 
 URL = 'https://bash.im/random'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:48.0) Gecko/20100101 Firefox/48.0'
