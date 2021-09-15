6c8dc1ef7d7b3e83badda5d8a7d51f2d6f79ef2d
gil9red <ilya.petrash@inbox.ru>
2021-09-15 15:55:52 +0500

    Update. Start again crawling pages in the 'download_seq_page_quotes' function

--- parsers.py
+++ parsers.py
@@ -130,55 +130,59 @@ def download_main_page_quotes(log: logging.Logger, dir_comics):
 def download_seq_page_quotes(log: logging.Logger, dir_comics):
     prefix = f'[{caller_name()}]'
 
+    i = 0
     while True:
-        i = 0
-        page = read_next_checked_page()
+        try:
+            page = read_next_checked_page()
 
-        while page > 0:
-            try:
-                with lock:
-                    count = db.Quote.select().count()
-                    log.debug(f'{prefix} Quotes: {count}')
-                    t = time.perf_counter_ns()
+            # Если дошли до последней страницы, начинаем заново
+            if page < 1:
+                log.debug(f'{prefix} Starting over again')
+                page = bash_im.get_total_pages()
 
-                    for quote in bash_im.get_page_quotes(page, log):
-                        # При отсутствии, цитата будет добавлена в базу
-                        db.Quote.get_from(quote)
+            with lock:
+                count = db.Quote.select().count()
+                log.debug(f'{prefix} Quotes: {count}')
+                t = time.perf_counter_ns()
 
-                        # Сразу же пробуем скачать комиксы
-                        quote.download_comics(dir_comics)
+                for quote in bash_im.get_page_quotes(page, log):
+                    # При отсутствии, цитата будет добавлена в базу
+                    db.Quote.get_from(quote)
 
-                    elapsed_ms = (time.perf_counter_ns() - t) // 1_000_000
-                    log.debug(
-                        f'{prefix} Added new quotes (page %s): %s, elapsed %s ms',
-                        page, db.Quote.select().count() - count, elapsed_ms
-                    )
+                    # Сразу же попробуем скачать комиксы
+                    quote.download_comics(dir_comics)
 
-                page -= 1
-                save_next_checked_page(page)
+                elapsed_ms = (time.perf_counter_ns() - t) // 1_000_000
+                log.debug(
+                    f'{prefix} Added new quotes (page %s): %s, elapsed %s ms',
+                    page, db.Quote.select().count() - count, elapsed_ms
+                )
 
-            except Exception:
-                log.exception(f'{prefix} Error:')
+            page -= 1
+            save_next_checked_page(page)
 
-                log.info(f"{prefix} I'll try again in 1 minute ...")
-                time.sleep(60)
+        except Exception:
+            log.exception(f'{prefix} Error:')
 
-            finally:
-                # 3 - 15 minutes
-                minutes = randint(3, 15)
-                log.debug(f'{prefix} Mini sleep: %s minutes', minutes)
+            log.info(f"{prefix} I'll try again in 1 minute ...")
+            time.sleep(60)
+
+        finally:
+            # 3 - 15 minutes
+            minutes = randint(3, 15)
+            log.debug(f'{prefix} Mini sleep: %s minutes', minutes)
+
+            time.sleep(minutes * 60)
+
+            i += 1
+            if i >= 10:
+                i = 0
+
+                # 3 - 6 hours
+                minutes = randint(3 * 60, 6 * 60)
+                log.debug(f'{prefix} Deep sleep: %s minutes', minutes)
 
                 time.sleep(minutes * 60)
-
-                i += 1
-                if i == 10:
-                    i = 0
-
-                    # 3 - 6 hours
-                    minutes = randint(3 * 60, 6 * 60)
-                    log.debug(f'{prefix} Deep sleep: %s minutes', minutes)
-
-                    time.sleep(minutes * 60)
 
 
 def run_parser_health_check(log: logging.Logger):
